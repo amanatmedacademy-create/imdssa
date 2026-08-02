65edbea916e8ca2bdba17b47d4c38e718b9d02ca
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-02 15:56:18 +0500

    feat: activate authentication provider and protected app gate

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
+import { AuthGate } from './core/AuthGate';
+import { AuthProvider } from './core/auth';
 import './styles.css';
 import './productRegistry.css';
+import './controlPlane.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <AuthGate>
+          <App />
+        </AuthGate>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
