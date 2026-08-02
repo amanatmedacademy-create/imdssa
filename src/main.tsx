80403c3150503f5b6614d853bb7984f5c135feb2
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-02 16:07:29 +0500

    feat: connect shared product catalog provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
 import { AuthGate } from './core/AuthGate';
 import { AuthProvider } from './core/auth';
+import { ProductCatalogProvider } from './features/products/ProductCatalogContext';
 import './styles.css';
 import './productRegistry.css';
 import './controlPlane.css';
+import './productCatalog.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <AuthGate>
-          <App />
+          <ProductCatalogProvider>
+            <App />
+          </ProductCatalogProvider>
         </AuthGate>
       </AuthProvider>
     </BrowserRouter>
