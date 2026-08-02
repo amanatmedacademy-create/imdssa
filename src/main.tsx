fb46995192c04d4c4ee4280b8cf02af9f9ed16ff
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-02 19:52:11 +0500

    chore: compose analytics provider with latest providers

--- src/main.tsx
+++ src/main.tsx
@@ -6,20 +6,26 @@ import { AuthGate } from './core/AuthGate';
 import { AuthProvider } from './core/auth';
 import { ProductAnalyticsProvider } from './features/analytics/ProductAnalyticsContext';
 import { BillingProvider } from './features/billing/BillingContext';
+import { BillingOperationsProvider } from './features/billingOperations/BillingOperationsContext';
+import { GovernanceProvider } from './features/governance/GovernanceContext';
 import { IdentityProvider } from './features/identity/IdentityContext';
 import { ObservabilityProvider } from './features/observability/ObservabilityContext';
 import { OperationsProvider } from './features/operations/OperationsContext';
 import { ProductCatalogProvider } from './features/products/ProductCatalogContext';
 import { SecurityProvider } from './features/security/SecurityContext';
+import { SupportProvider } from './features/support/SupportContext';
 import './styles.css';
 import './productRegistry.css';
 import './controlPlane.css';
 import './productCatalog.css';
 import './billing.css';
+import './billingOperations.css';
 import './operations.css';
 import './security.css';
 import './observability.css';
 import './productAnalytics.css';
+import './support.css';
+import './governance.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -28,17 +34,23 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <AuthGate>
           <ProductCatalogProvider>
             <BillingProvider>
-              <IdentityProvider>
-                <OperationsProvider>
-                  <SecurityProvider>
-                    <ObservabilityProvider>
-                      <ProductAnalyticsProvider>
-                        <App />
-                      </ProductAnalyticsProvider>
-                    </ObservabilityProvider>
-                  </SecurityProvider>
-                </OperationsProvider>
-              </IdentityProvider>
+              <BillingOperationsProvider>
+                <IdentityProvider>
+                  <OperationsProvider>
+                    <SecurityProvider>
+                      <ObservabilityProvider>
+                        <ProductAnalyticsProvider>
+                          <SupportProvider>
+                            <GovernanceProvider>
+                              <App />
+                            </GovernanceProvider>
+                          </SupportProvider>
+                        </ProductAnalyticsProvider>
+                      </ObservabilityProvider>
+                    </SecurityProvider>
+                  </OperationsProvider>
+                </IdentityProvider>
+              </BillingOperationsProvider>
             </BillingProvider>
           </ProductCatalogProvider>
         </AuthGate>
