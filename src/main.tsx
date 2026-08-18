421fbdd6e924ad4f43e6185d308c5685a95ac00d
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-18 05:29:40 +0500

    mount active session management in Control Center

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import { UserAccessOverlay } from './features/access/UserAccessOverlay';
 import { NotificationBell } from './features/notifications/NotificationBell';
 import { ProductCommercialBridge } from './features/products/ProductCommercialBridge';
+import { SessionManagementBridge } from './features/security/SessionManagementBridge';
 import './notifications.css';
 import { installControlCenterBranding } from './vps/branding';
 import { VpsApp } from './vps/VpsApp';
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(rootElement);
 
 if (import.meta.env.VITE_RUNTIME === 'vps') {
   installControlCenterBranding(rootElement);
-  root.render(<><VpsApp /><NotificationBell /><UserAccessOverlay /><ProductCommercialBridge /></>);
+  root.render(<><VpsApp /><NotificationBell /><UserAccessOverlay /><ProductCommercialBridge /><SessionManagementBridge /></>);
 } else {
   void import('./LegacyApp').then(({ LegacyApp }) => {
     root.render(<LegacyApp />);
