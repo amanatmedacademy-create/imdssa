dfcd8a969904cf6ce383d5462fd15bfbe794a388
amanatmedacademy-create <amanat.med.academy@gmail.com>
2026-08-18 16:02:04 +0500

    serve control center v2 at production root

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,9 @@
 import ReactDOM from 'react-dom/client';
-import { UserAccessOverlay } from './features/access/UserAccessOverlay';
-import { VpsBillingBridge } from './features/billing/VpsBillingBridge';
-import { NotificationBell } from './features/notifications/NotificationBell';
-import { ProductCommercialBridge } from './features/products/ProductCommercialBridge';
-import { SessionManagementBridge } from './features/security/SessionManagementBridge';
-import { OrganizationSubscriptionBridge } from './features/subscriptions/OrganizationSubscriptionBridge';
-import './notifications.css';
-import { installControlCenterBranding } from './vps/branding';
 import { InfrastructureCenter } from './vps/InfrastructureCenter';
 import { ControlCenterV2 } from './vps/pages/ControlCenterV2';
 import { OverviewPreviewApp } from './vps/pages/overview/OverviewPreviewApp';
-import { VpsApp } from './vps/VpsApp';
+import { RootApp } from './vps/pages/RootApp';
+import { installControlCenterBranding } from './vps/branding';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Root element is missing');
@@ -22,17 +15,8 @@ if (window.location.pathname === '/control-center-v2') {
   root.render(<ControlCenterV2 />);
 } else if (window.location.pathname === '/overview-v2') {
   root.render(<OverviewPreviewApp />);
+} else if (window.location.pathname === '/infrastructure') {
+  root.render(<InfrastructureCenter />);
 } else {
-  root.render(
-    <>
-      <VpsApp />
-      <InfrastructureCenter />
-      <NotificationBell />
-      <UserAccessOverlay />
-      <ProductCommercialBridge />
-      <OrganizationSubscriptionBridge />
-      <VpsBillingBridge />
-      <SessionManagementBridge />
-    </>,
-  );
+  root.render(<RootApp />);
 }
